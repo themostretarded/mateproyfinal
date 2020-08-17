0f4e2cae0ad2c3405bf48c68811359984edccda2
themostretarded <marcosresidentevil@hotmail.com>
2020-08-17 03:33:52 -0500

    Reajustando
    Movimos una matrz a no ver
    añadi las referencias cuales usare para el documento
    obtener arista random funciona
    guardando en caso de que falle laptop, esta actuando rara

--- algoritmo.js
+++ algoritmo.js
@@ -1,3 +1,13 @@
 let matriz = require("./lectura")
 
-console.log(matriz);
+console.log(matriz.length);
+
+/* random para comenzar algoritmo
+console.log( Math.floor(Math.random() * 6) + 1  ) 
+empezaremos igual al ejemplo para ver si funciona
+https://sites.google.com/site/complejidadalgoritmicaes/prim
+https://es.wikipedia.org/wiki/Algoritmo_de_Prim
+https://es.wikipedia.org/wiki/Grafo_conexo
+https://es.wikipedia.org/wiki/%C3%81rbol_recubridor_m%C3%ADnimo
+
+*/
